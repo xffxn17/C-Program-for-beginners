93d30d076910ce54a7a04a6f03ede22f4a8b8e86
affan <mohdaffan.hacker@gmail.com>
2025-09-06 20:33:55 +0530

    maked the changes in this file

--- array3.c
+++ array3.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // write a program to enter the elements of an array and determine the
 // sum of an array?
 #include <stdio.h>
@@ -12,19 +11,4 @@ void main()
         sum = sum + a[i];
     }
     printf("Sum of all the elements is: %d", sum);
-=======
-//write a program to enter the elements of an array and determine the 
-//sum of an array?
-#include<stdio.h>
-void main()
-{
-    int a[5],i,sum=0;
-    printf("Enter the elements of an array:\n");
-    for(i=0;i<5;i++)
-{
-    scanf("%d",&a[i]);
-    sum=sum+a[i];
-}
-printf("Sum of all the elements is: %d",sum);
->>>>>>> 726c1b5be6fdfe47c300692f37ff231c349e9d00
 }
